8a539d26d6e6f246732034449b7f964f176c89da
Roberto Venezia <robertovenezia95@gmail.com>
2024-06-19 12:52:59 +0200

    Steam integration

--- RTS_Project/Assets/Scripts/RTS/UI/MainMenu.cs
+++ RTS_Project/Assets/Scripts/RTS/UI/MainMenu.cs
@@ -2,15 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using Steamworks;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject landingPagePanel;
+    [SerializeField] private bool usingSteam = false;
 
+    private Callback<LobbyCreated_t> lobbyCreated;
+    private Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    private Callback<LobbyEnter_t> lobbyEntered;
+
+    private void Start()
+    {
+        if (!usingSteam) return;
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
+
+   
     public void HostLobby()
     {
         landingPagePanel.SetActive(false);
 
+        if(usingSteam)
+        {
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            return;
+        }
+
         NetworkManager.singleton.StartHost();
     }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if(callback.m_eResult != EResult.k_EResultOK)
+        {
+            landingPagePanel.SetActive(true);
+            return;
+        }
+
+        NetworkManager.singleton.StartHost();
+
+        SteamMatchmaking.SetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            "HostAddress", 
+            SteamUser.GetSteamID().ToString());
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active) return;
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(
+            new CSteamID(callback.m_ulSteamIDLobby),
+            "HostAddress");
+
+        NetworkManager.singleton.networkAddress = hostAddress;
+        NetworkManager.singleton.StartClient();
+
+        landingPagePanel.SetActive(false);
+    }
+
 }
